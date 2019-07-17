9a6746e8a0ed499f8bae7b2cfe7d0388591d4dcd
ddot727 <ddot727@gmail.com>
2019-07-17 18:03:54 -0400

    added functionality thanks to a tutorial, still working on controls

--- gameoflife/src/components/Board.js
+++ gameoflife/src/components/Board.js
@@ -5,14 +5,14 @@ import '../App.css';
 class Board extends Component {
   render() {
     const width = (this.props.cols * 16);
-    var rowsArr = [];
+    const rowsArr = [];
 
     var boxClass = "";
     for (var i = 0; i < this.props.rows; i++) {
       for (var j = 0; j < this.props.cols; j++) {
         let boxId = i + "_" + j;
 
-        boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+        boxClass = this.props.boardFull[i][j] ? "box on" : "box off";
         rowsArr.push(
           <Box
             boxClass={boxClass}
--- gameoflife/src/components/Game.js
+++ gameoflife/src/components/Game.js
@@ -5,12 +5,82 @@ import '../App.css';
 class Game extends Component {
   constructor() {
     super();
+    this.speed = 100;
     this.rows = 30;
     this.cols = 30;
     this.state = {
       generation: 0,
-      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+      boardFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
     };
+  }
+
+  selectBox = (row, col) => {
+    let boardCopy = arrayClone(this.state.boardFull);
+    boardCopy[row][col] = !boardCopy[row][col];
+    this.setState({
+      boardFull: boardCopy
+    });
+  }
+
+  seed = () => {
+    let boardCopy = arrayClone(this.state.boardFull);
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        if (Math.floor(Math.random() * 4) === 1) {
+          boardCopy[i][j] = true;
+        }
+      }
+    }
+    this.setState({
+      boardFull: boardCopy
+    });
+  }
+
+  playButton = () => {
+    clearInterval(this.intervalId);
+    this.intervalId = setInterval(this.play, this.speed);
+  }
+
+  pauseButton = () => {
+    clearInterval(this.intervalId);
+  }
+
+  play = () => {
+    let g = this.state.boardFull;
+    let g2 = arrayClone(this.state.boardFull);
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        let count = 0;
+        if (i > 0) if (g[i - 1][j]) count++;
+        if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+        if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+        if (j < this.cols - 1) if (g[i][j + 1]) count++;
+        if (j > 0) if (g[i][j - 1]) count++;
+        if (i < this.rows - 1) if (g[i + 1][j]) count++;
+        if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+        if (i < this.rows - 1 && j < this.cols - 1) if (g[i + 1][j + 1]) count++;
+        if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+        if (!g[i][j] && count === 3) g2[i][j] = true;
+      }
+    }
+    this.setState({
+      boardFull: g2,
+      generation: this.state.generation + 1
+    });
+
+  }
+
+  stop = () => {
+    var board = Array(this.rows).fill().map(() => Array(this.cols).fill(false));
+    this.setState({
+      boardFull: board,
+      generation: 0
+    });
+  }
+
+  componentDidMount() {
+    this.seed();
   }
 
   render() {
@@ -18,19 +88,23 @@ class Game extends Component {
       <div>
         <h2>Generation: # {this.state.generation}</h2>
         <Board
-          gridFull={this.state.gridFull}
+          boardFull={this.state.boardFull}
           rows={this.rows}
           cols={this.cols}
           selectBox={this.selectBox}
         />
         <div className="buttonGroup">
-          <button>Play</button>
-          <button>Pause</button>
-          <button>Stop</button>
+          <button onClick={this.playButton}>Play</button>
+          <button onClick={this.pauseButton}>Pause</button>
+          <button onClick={this.stop}>Stop</button>
         </div>
       </div>
     );
   }
+}
+
+function arrayClone(arr) {
+  return JSON.parse(JSON.stringify(arr));
 }
 
 export default Game; 
